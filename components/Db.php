2900f80e4eb639c81e0fe952842d0dbbd248d3c1
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:44:49 +0300

    Refactoring OPP style, Connection to BD with UTF-8

--- components/Db.php
+++ components/Db.php
@@ -10,13 +10,18 @@ class Db
 {
     public static function getConnection()
     {
+
         $paramsPath = ROOT . '/config/dbParams.php';
         $params = include($paramsPath);
+        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
 
         try {
-            $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-            $db = new PDO($dsn, $params['user'], $params['password']);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db = new PDO($dsn,
+                $params['user'],
+                $params['password'],
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+            $db->exec("set names utf8");
+
         } catch (PDOException $e) {
             echo 'Подключение к БД не удалось: ' . $e->getMessage();
             die();
