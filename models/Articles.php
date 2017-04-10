e38398056b0d32a4fddbb1e500e31a8332943d2e
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:36:56 +0300

    Refactoring OPP style, Addition of Articles class

--- models/Articles.php
+++ models/Articles.php
@@ -2,34 +2,63 @@
 
 class Articles
 {
-    public static function getArticleById($id)
+    public static function getArticleById($titleTranslit)
     {
         $db = Db::getConnection();
-        $id = (int)$id;
+        $category = trim($titleTranslit);
 
-        $result = $db->query("SELECT * FROM articles WHERE id=" . $id);
+
+        $result = $db->prepare("SELECT * FROM articles WHERE title_translit=" . "'$titleTranslit'");
+        $result->execute([$category]);
         $result->setFetchMode(PDO::FETCH_ASSOC);
 
         $articleItem = $result->fetch();
 
         return $articleItem;
-
     }
 
-    public static function getArticleList($category=null)
+    public static function incrementViews($articleID)
+    {
+        $db = Db::getConnection();
+        $articleID = (int)$articleID;
+        $db->query("UPDATE articles SET views = views + 1 WHERE id = " . $articleID);
+
+        return true;
+    }
+
+    public static function getArticleList($perPage, $category = null, $offset = null)
     {
         $db = Db::getConnection();
         $category = trim($category);
         $articleList = array();
 
-        if ($category!=null) {
-            $result = $db->prepare("SELECT * FROM articles WHERE category_id=(SELECT id FROM articles_categories WHERE title_translit=?) ORDER BY id DESC LIMIT 10");
-            $result->execute([$category]);
+        if ($offset != null) {
 
-            // $result = $db->query("SELECT * FROM articles WHERE category_id=(SELECT id FROM articles_categories WHERE title_translit=?) ORDER BY id DESC LIMIT 10");
+            if ($category != null) {
+                $result = $db->prepare("SELECT * FROM articles WHERE category_id=
+                                  (SELECT id FROM articles_categories 
+                                  WHERE title_translit=?) 
+                                  ORDER BY id DESC LIMIT " . $offset . "," . $perPage);
+
+                $result->execute([$category]);
+
+            } else {
+                $result = $db->query("SELECT * FROM articles ORDER BY id DESC LIMIT $offset,$perPage");
+
+            }
 
         } else {
-            $result = $db->query("SELECT * FROM articles ORDER BY id DESC LIMIT 10");
+            if ($category != null) {
+                $result = $db->prepare("SELECT * FROM articles WHERE category_id=
+                                  (SELECT id FROM articles_categories 
+                                  WHERE title_translit=?) 
+                                  ORDER BY id DESC LIMIT " . $perPage);
+
+                $result->execute([$category]);
+
+            } else {
+                $result = $db->query("SELECT * FROM articles ORDER BY id DESC LIMIT " . $perPage);
+            }
         }
         $result->setFetchMode(PDO::FETCH_ASSOC);
 
@@ -38,6 +67,28 @@ class Articles
         }
 
         return $articleList;
+    }
+
+    public static function getTotalCountOfArticle($category = null)
+    {
+        $db = Db::getConnection();
+        $category = trim($category);
+
+        if ($category != null) {
+            $result = $db->prepare("SELECT COUNT(id) as total_count FROM articles WHERE category_id=
+                                  (SELECT id FROM articles_categories 
+                                  WHERE title_translit=?)");
+            $result->execute([$category]);
+        } else {
+            $result = $db->query("SELECT COUNT(id) as total_count FROM articles");
+        }
+
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+
+        $totalCount = $result->fetch();
+        $totalCount = $totalCount['total_count'];
+
+        return $totalCount;
     }
 
     public static function getArticleToSidebar()
@@ -58,31 +109,15 @@ class Articles
     public static function getTitleTranslitOfArticle($id)
     {
         $db = Db::getConnection();
-        $id=(int)$id;
+        $id = (int)$id;
 
-        $result = $db->query("SELECT title_translit FROM articles WHERE id=".$id);
+        $result = $db->query("SELECT title_translit FROM articles WHERE id=" . $id);
 
         $result = $result->fetch();
 
         $result = $result['title_translit'];
 
         return $result;
-    }
-    /* public static function switchFunction($getFunction){
-         switch ($status) {
-             case '1':
-                 return 'Отображается';
-                 break;
-             case '0':
-                 return 'Скрыта';
-                 break;
-         }
-     }*/
-
-    public static function searchArticle()
-    {
-        $db = Db::getConnection();
-        $articleList = array();
     }
 
 
