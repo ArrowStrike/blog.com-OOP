b57aa46e4f625cbc8f8382f54ba1cf662df123bd
Vladislavs <kawasak2@gmail.com>
2017-04-10 04:01:02 +0300

    Code formatting

--- admin/models/start.php
+++ admin/models/start.php
@@ -9,7 +9,7 @@ session_start();
 require_once "functions.php";
 require_once "database.php";
 $link = db_connect();
-if (!((checkUser($link, $_SESSION["email"], $_SESSION["password"])) && (isAdmin($link,$_SESSION["email"])))) {
+if (!((checkUser($link, $_SESSION["email"], $_SESSION["password"])) && (isAdmin($link, $_SESSION["email"])))) {
     redirect("/admin/views/authPage.php");
     exit;
 }
