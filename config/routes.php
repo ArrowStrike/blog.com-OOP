ea00dc632e4e83a2087eb1cc4f60daa6684b5855
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:58:45 +0300

    Readable

--- config/routes.php
+++ config/routes.php
@@ -2,22 +2,29 @@
 
 return array(
     //   запрос что набирает пользователь/строка по которой мы узнаем, где обрабатывается запрос
-//  'category/([0-9]+)-([0-9a-z_]+)'=>'category/view/$1',
-
-    'aboutMe'=> 'aboutMe/index',
-    'copyright'=> 'copyright/index',
-    'search?q=(.*)'=> 'articles/search/$1',
-    'article/([0-9a-z_]+)'=> 'article/index/$1',
-
-    'articles/page-([0-9]+)' => 'articles/$1', // actionCategory в CatalogController
-    'articles' => 'articles/index', //будет вызван метод actionIndex в контроллере ArticlesController
-
-    '([0-9a-z_]+)/page-([0-9]+)' => 'articles/category/$1/$2', // actionCategory в CatalogController
-    '([0-9a-z_]+)'=>'articles/index/$1',
+    // => controller / action
 
 
-    ''=>'site/index',
-    //  'products' => 'products/list', //actionList в контроллере ProductController
+    'aboutMe' => 'aboutMe/index',
+    'copyright' => 'copyright/index',
+
+    //поиск
+    'search(.*)/page-([0-9]+)' => 'articles/search/$2',
+    'search(.*)' => 'articles/search',
+
+    //страница статьи
+    'article/([0-9a-z_]+)' => 'article/index/$1',
+
+    //страница всех стетей
+    'articles/page-([0-9]+)' => 'articles/index//$1',
+    'articles' => 'articles/index',
+
+    //категории
+    '([0-9a-z_]+)/page-([0-9]+)' => 'articles/index/$1/$2',
+    '(.+)' => 'articles/index/$1',
+
+    //начальная страница
+    '' => 'site/index',
 
 
 );
