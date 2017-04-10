ea092184ad77a1469c70f497331756d356389f76
Vladislavs <kawasak2@gmail.com>
2017-04-10 04:00:12 +0300

    Code formatting

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,9 +5,9 @@ class SiteController
     public function actionIndex()
     {
         $categories = Category::getCategoryList();
-        $articleList = Articles::getArticleList();
+        $articleList = Articles::getArticleList(8);
         $articleToSidebar = Articles::getArticleToSidebar();
-        $commentsToSidebar= Comments::getCommentsToSidebar();
+        $commentsToSidebar = Comments::getCommentsToSidebar();
         require_once(ROOT . '/views/site/index.php');
         return true;
     }
