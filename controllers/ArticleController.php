6c26691911eadc9cfdddf8b7f1b12f65740b0d84
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:31:10 +0300

    Refactoring OPP style, Addition of article controller

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -9,10 +9,18 @@
 class ArticleController
 {
     //"Просмотре одной новости";
-    public function actionIndex($id)
+    public function actionIndex($titleTranslit)
     {
-        if ($id){
-            $articleItem = Articles::getArticleById($id);
+        if ($titleTranslit) {
+            $categories = Category::getCategoryList();
+            $articleToSidebar = Articles::getArticleToSidebar();
+            $articleItem = Articles::getArticleById($titleTranslit);
+            Articles::incrementViews($articleItem['id']);
+            $logsOfValidation = Comments::commentsValidator($articleItem['id']);
+            $commentsToSidebar = Comments::getCommentsToSidebar();
+            $commentsList = Comments::getComments($articleItem['id']);
+
+            require_once(ROOT . '/views/article/index.php');
         }
         return true;
     }
