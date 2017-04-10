850fe7e2c4ed35f1f9f927fc644cc40b41a9c59a
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:37:30 +0300

    Refactoring OPP style, Addition of Articles controller

--- controllers/ArticlesController.php
+++ controllers/ArticlesController.php
@@ -9,20 +9,47 @@
 class ArticlesController
 {
     //'Список статей';
-    public function actionIndex($category=null)
+    public function actionIndex($category = null, $page = 1)
     {
         $categories = Category::getCategoryList();
-        $articleList = Articles::getArticleList($category);
+        if ($page == 0) {
+            $page = 1;
+        }
+        $offset = (Configs::getConfig('articlePerPage') * $page) - Configs::getConfig('articlePerPage'); //сдвиг
+        $articleList = Articles::getArticleList(Configs::getConfig('articlePerPage'), $category, $offset);
+
+        // Общее количетсво статей (необходимо для постраничной навигации)
+        $total = Articles::getTotalCountOfArticle($category);
+
+        // Создаем объект Pagination - постраничная навигация
+        $pagination = new Pagination($total, $page, Configs::getConfig('articlePerPage'), 'page-');
+
         $articleToSidebar = Articles::getArticleToSidebar();
-        $commentsToSidebar= Comments::getCommentsToSidebar();
+        $commentsToSidebar = Comments::getCommentsToSidebar();
+
         require_once(ROOT . '/views/articles/index.php');
         return true;
     }
 
-    public function actionSearch($query)
+
+    public function actionSearch($page = 1)
     {
-        if ($query) {
-            $searchResult = Articles::searchArticle($query);
+
+        if ($_GET['q']) {
+            $categories = Category::getCategoryList();
+            if ($page == 0) {
+                $page = 1;
+            }
+            $offset = (Configs::getConfig('articlePerPage') * $page) - Configs::getConfig('articlePerPage'); //сдвиг
+            $articleList = Search::getSearchList(Configs::getConfig('articlePerPage'), $offset);
+
+            $keyWord = Search::getKeyWord();
+            $total = Search::getTotalCountOfSearch($keyWord);
+            $pagination = new Pagination($total, $page, Configs::getConfig('articlePerPage'), 'page-');
+
+            $articleToSidebar = Articles::getArticleToSidebar();
+            $commentsToSidebar = Comments::getCommentsToSidebar();
+
             require_once(ROOT . '/views/articles/index.php');
         }
         return true;
