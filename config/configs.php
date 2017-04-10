8b4b12b914cf87ebe0e2135ff4299f5735de35fe
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:43:23 +0300

    New variable PerPage

--- config/configs.php
+++ config/configs.php
@@ -1,9 +1,10 @@
 <?php
 return array(
-    'version' => 2,
+    'version' => 3,
     'title' => 'Блог Влада',
     'vk_url' => 'https://vk.com/id52976219',
     'github_url' => 'https://github.com/ArrowStrike/blog.com-OOP',
     'linkedIn_url' => 'https://www.linkedin.com/in/vladislavs-tarasenkovs/',
+    'articlePerPage' => 4,
 );
 
