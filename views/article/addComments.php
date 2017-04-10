855ba6ef84ad462527ea897382c88b2de02bedcc
Vladislavs <kawasak2@gmail.com>
2017-04-10 03:27:19 +0300

    Refactoring OPP style

--- views/article/addComments.php
+++ views/article/addComments.php
@@ -1,39 +1,31 @@
 <div class="block" id="comment-add-form">
     <h3>Добавить комментарий</h3>
-
     <div class="block__content">
         <form class="form" method="POST"
-              action="/article/<?php echo $art['id'] . "-" . translit($art['title']); ?>#comment-add-form">
-            <?php if (empty($errors)) {
-                echo '<span style="color: green; font-weight:bold; margin-bottom: 10px; display: block; " > 
-                        Комментарий успешно добавлен!
-                      </span>';
-            } else {
-                echo '<span style="color: red; font-weight:bold; margin-bottom: 10px; display: block; "> '
-                    . $errors['0'] .
-                     '</span>';
-            }
-            ?>
+              action="/article/<?php echo $articleItem['title_translit']; ?>#comment-add-form">
+
+            <?php Comments::setResultOfValidation($logsOfValidation); ?>
+
             <div class="form__group">
                 <div class="row">
                     <div class="col-md-6">
                         <input type="text" class="form__control" name="name" placeholder="Имя"
-                               value="<?php echo $postData['name']; ?>">
+                               value="<?php echo $logsOfValidation['commentUserName'] ?>">
                     </div>
                     <div class="col-md-6">
                         <input type="text" class="form__control" name="email"
                                placeholder="Email (Не будет показан)"
-                               value="<?php echo $postData['email']; ?>">
+                               value="<?php echo $logsOfValidation['commentUserEmail'] ?>">
                     </div>
                 </div>
             </div>
             <div class="form__group">
                <textarea name="text" class="form__control"
-                         placeholder="Текст комментария ..."><?php echo $postData['text']; ?></textarea>
+                         placeholder="Текст комментария ..."><?php echo $logsOfValidation['commentText'] ?></textarea>
             </div>
 
             <div class="form__group">
-                <input type="submit" class="form__control" name="do_post"
+                <input type="submit" class="form__control" name="submit"
                        value="Добавить комментарий">
             </div>
 
