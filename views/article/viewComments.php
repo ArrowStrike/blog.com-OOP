cb47eb5c38e60966bc8bacdbff839fa5a129793d
Vladislavs <kawasak2@gmail.com>
2017-04-10 04:01:38 +0300

    Refactoring OPP style, Code formatting

--- views/article/viewComments.php
+++ views/article/viewComments.php
@@ -4,71 +4,31 @@
     <div class="block__content">
         <div class="articles articles__vertical">
             <?php
-            $errors = array(null);
-            $postData = $_POST;
-            if (!empty($postData))//если была нажата кнопка
-            {
-                $errors = array();
-                if ($postData['name'] == '') {
-                    $errors[] = 'Введите имя!';
-                }
-                /* if ($postData['nickname'] == '') {
-                       $errors[] = 'Введите Ваш никнейм!';
-                   }*/
-                if ($postData['email'] == '') {
-                    $errors[] = 'Введите Email!';
-                }
-                if ((filter_var($postData['email'], FILTER_VALIDATE_EMAIL)) == false) {
-                    $errors[] = 'Введите правильный Email!';
-                }
-                if ($postData['text'] == '') {
-                    $errors[] = 'Введите текст комментария!';
-                }
-                if (empty($errors)) {
-                    mysqli_query($connection, "INSERT INTO comments (author, nickname, email, text, pubdate, articles_id) 
-                                              VALUES ('" . $postData['name'] . "',
-                                              '" . $postData['nickname'] . "',
-                                              '" . $postData['email'] . "',
-                                              '" . $postData['text'] . "',
-                                               NOW(),
-                                              '" . $art['id'] . "')");
-                    //добавить коммент
-
-                    unset($_POST);
-                    unset($postData);
-                    //   $zaza= "http://blog.com/pages/article.php?id=".$art['id']."#comment-add-form";
-                    //   echo "<script language='JavaScript' type='text/javascript'>window.location.replace('zaza')</script>";
-                    //       var_dump($POST['text']);
-                    //       die();
-                }
-            }
-            ?>
-            <?php
-            $comments = mysqli_query($connection, "SELECT * FROM comments 
-                                                  WHERE articles_id = " . (int)$art['id'] . " ORDER BY id DESC");
-            if (mysqli_num_rows($comments) <= 0) {
+            if ($commentsList == null) {
                 echo "Нет комментариев!";
-            }
-            while ($comment = mysqli_fetch_assoc($comments)) {
-                ?>
-                <article class="article">
-                    <div class="article__image"
-                         style="background-image:
-                             url(https://www.gravatar.com/avatar/<?php echo md5($comment['email']); ?>?s=125);">
-                    </div>
-                    <div class="article__info">
-                        <a href="/article/<?php echo $comment['articles_id']; ?>">
-                            <?php echo $comment['author']; ?>
-                        </a>
-                        <div class="article__info__meta">
-                            <?php echo date('H:i d-m-y', strtotime($comment['pubdate'])); ?>
+            } else {
+                foreach ($commentsList as $comment) {
+                    ?>
+                    <article class="article">
+                        <div class="article__image"
+                             style="background-image:
+                                 url(https://www.gravatar.com/avatar/<?php echo md5($comment['email']); ?>?s=125);">
                         </div>
-                        <div class="article__info__preview">
-                            <?php echo $comment['text']; ?>
+                        <div class="article__info">
+                            <a href="/article/<?php
+                            echo Articles::getTitleTranslitOfArticle($comment['articles_id']); ?>">
+                                <?php echo $comment['author']; ?>
+                            </a>
+                            <div class="article__info__meta">
+                                <?php echo date('H:i d-m-y', strtotime($comment['pubdate'])); ?>
+                            </div>
+                            <div class="article__info__preview">
+                                <?php echo $comment['text']; ?>
+                            </div>
                         </div>
-                    </div>
-                </article>
-                <?php
+                    </article>
+                    <?php
+                }
             }
             ?>
 
